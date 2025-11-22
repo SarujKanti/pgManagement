e6609b97f45d78d6196e52ac9cc7eb1dbe6bed60
saruj <14skd12@gmail.com>
2025-11-22 21:11:20 +0530

    changes

--- app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
+++ app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
@@ -8,6 +8,7 @@ import androidx.viewbinding.ViewBinding
 class GenericAdapter<T , VB: ViewBinding>(
     private var data: MutableList<T>,
     private val bind: (VB, T, Int) -> Unit,
+    
     private val inflater: (LayoutInflater, ViewGroup, Boolean) -> VB
 ) : RecyclerView.Adapter<GenericAdapter.GenericViewHolder<VB>>() {
 
