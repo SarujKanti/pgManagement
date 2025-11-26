57796a1edbd5711e1d9a880a6e93ccbeb1b0d186
saruj <14skd12@gmail.com>
2025-11-26 10:30:49 +0530

    extra

--- app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
+++ app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
@@ -18,7 +18,6 @@ class GenericAdapter<T, VB : ViewBinding>(
         val binding = inflater(LayoutInflater.from(parent.context), parent, false)
         return GenericViewHolder(binding)
     }
-
     override fun onBindViewHolder(holder: GenericViewHolder<VB>, position: Int) {
         bind(holder.binding, data[position], position)
     }
