8b7c153a033ec3b8967227d5e0cfeaea07ef7180
saruj <14skd12@gmail.com>
2025-12-16 22:05:47 +0530

    changes

--- app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
+++ app/src/main/java/com/skd/pgmanagement/adapters/GenericAdapter.kt
@@ -22,7 +22,7 @@ class GenericAdapter<T, VB : ViewBinding>(
     override fun onBindViewHolder(holder: GenericViewHolder<VB>, position: Int) {
         bind(holder.binding, data[position], position)
     }
-    
+
     override fun getItemCount(): Int = data.size
     fun updateItem(position: Int, newItem: T) {
         if (position in data.indices) {
